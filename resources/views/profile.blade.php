09fe3c31808d7ffb081d81c851ac5fc73d0134f3
gaurav <gaurav1katiyar2011@gmail.com>
2018-07-08 01:03:40 +0530

    Adde Authentication + First route - profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Profile</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ auth()->user()->name }}
+                        </div>
+                    @endif
+
+                    {{ auth()->user()->name }}<br/>
+                    {{ auth()->user()->id }}
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
